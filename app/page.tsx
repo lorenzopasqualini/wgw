d2662abbd39361ba703d4df769f21aa546465bdf
Lorenzo Pasqualini <lorpasqu@gmail.com>
2023-12-01 16:54:54 +0100

    v0.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image'
+import { fetchData } from './action'
+import Card, { ImgProp } from '@/components/Card'
 
-export default function Home() {
+export default async function Home() {
+  const data= await fetchData(1)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+      {data.map((item: ImgProp, index:number) => (
+          <Card key={item.id} series={item} index={index} />
+        ))}
+      </div>
     </main>
   )
 }
---
+++ components/Card.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+export interface ImgProp {
+  id: string;
+  name: string;
+  image: {
+    original: string;
+  };
+  kind: string;
+  episodes: number;
+  episodes_aired: number;
+  score: string;
+}
+
+interface Prop {
+  series: ImgProp;
+  index: number;
+}
+
+function Card({ series }: Prop) {
+  return (
+    <div className="max-w-sm rounded relative w-full">
+      <div className="relative w-full h-[37vh]">
+        <Image
+          src={`https://shikimori.one${series.image.original}`}
+          alt={series.name}
+          fill
+          className="rounded-xl"
+        />
+      </div>
+      <div className="py-4 flex flex-col gap-3">
+        <div className="flex justify-between items-center gap-1">
+          <h2 className="font-bold text-white text-xl line-clamp-1 w-full">
+            {series.name}
+          </h2>
+          <div className="py-1 px-2 bg-[#161921] rounded-sm">
+            <p className="text-white text-sm font-bold capitalize">
+              {series.kind}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
